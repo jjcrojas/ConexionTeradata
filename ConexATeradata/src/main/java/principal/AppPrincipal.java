26c47ce6a89b16a3887985a58d1e0c2bd3cdfcca
jcrojas <jcrojas@JCROJASP.superfinanciera.loc>
2024-10-18 15:41:56 -0500

    Clase AppPrincipal copiada

--- ConexATeradata/src/main/java/principal/AppPrincipal.java
+++ ConexATeradata/src/main/java/principal/AppPrincipal.java
@@ -3,8 +3,6 @@ package principal;
 public class AppPrincipal {
 
 	    public static void main(String[] args) {
-	        System.out.println("Hello, World!");
+	        System.out.println("Hello, Wild World!");
 	    }
-
-
 }
